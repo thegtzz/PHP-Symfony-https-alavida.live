c118d5bbe6ed19f86a2a36d5b8483b58ac863298
thegtzz <thegtzz@gmail.com>
2019-11-05 16:54:51 +0300

    Post now has category list in sonata admin

--- src/Admin/PostAdmin.php
+++ src/Admin/PostAdmin.php
@@ -1,10 +1,13 @@
 <?php
 namespace App\Admin;
 
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 final class PostAdmin extends AbstractAdmin
@@ -14,8 +17,15 @@ final class PostAdmin extends AbstractAdmin
         $formMapper
             ->add('title', TextType::class)
             ->add('body', TextType::class)
-            ->add('category', TextType::class);
-
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'query_builder' => function(CategoryRepository $er) {
+                    return $er->createQueryBuilder('c')
+                        ->orderBy('c.name', 'ASC');
+                },
+                'choice_label' => 'name',
+                'choice_value' => 'id',
+            ]);
     }
 
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
@@ -24,7 +34,6 @@ final class PostAdmin extends AbstractAdmin
             ->add('title')
             ->add('body')
             ->add('category');
-
     }
 
     protected function configureListFields(ListMapper $listMapper)
@@ -33,7 +42,5 @@ final class PostAdmin extends AbstractAdmin
             ->addIdentifier('title')
             ->addIdentifier('body')
             ->addIdentifier('category');
-
-
     }
 }
--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -82,4 +82,8 @@ class Category
 
         return $this;
     }
+
+    public function __toString() {
+        return $this->getName();
+    }
 }
